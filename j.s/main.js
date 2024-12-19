e0db8c0ba2a76abe9a19e137a0ffa642e959eef1
Dima121880 <surguladze.dima33@gmail.com>
2024-12-20 02:52:17 +0400

    add personal and mobile numbers

--- j.s/main.js
+++ j.s/main.js
@@ -1,1 +1,12 @@
-      
+const ispersonalValidFn = () => {
+  const personalValue = personalnumberInput.value.trim();
+  if (personalValue === "") {
+    setErrorMessage(personalnumberInput, "Personalnumber required");
+    if (personalValue.length < 1)
+      //   } else if (personalValue.length < 1) {
+      setErrorMessage(
+        personalnumberInput,
+        "personalnumber must be at least 6 characters"
+      );
+  }
+};
