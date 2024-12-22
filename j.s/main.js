a34135bca2f47f528e77b990070eafde8775c796
Dima121880 <surguladze.dima33@gmail.com>
2024-12-23 03:02:23 +0400

    add button

--- j.s/main.js
+++ j.s/main.js
@@ -1,12 +1,24 @@
-const ispersonalValidFn = () => {
-  const personalValue = personalnumberInput.value.trim();
-  if (personalValue === "") {
-    setErrorMessage(personalnumberInput, "Personalnumber required");
-    if (personalValue.length < 1)
-      //   } else if (personalValue.length < 1) {
-      setErrorMessage(
-        personalnumberInput,
-        "personalnumber must be at least 6 characters"
-      );
+const setErrorMessage = (input, message) => {
+  const parentEl = input.closest("Phonenumber");
+  parentEl.querySelector(".error").innerText = message;
+  parentEl.classList.add("fail");
+  parentEl.classList.remove("success");
+};
+const isPhoneNumberValidFn = () => {
+  const phoneNumberValue = phoneNumberInput.value.trim();
+  if (phoneNumberValue === "") {
+    setErrorMessage(phoneNumberInput, "Phone number is required");
+  } else if (!/^\d{9}$/.test(phoneNumberValue)) {
+    setErrorMessage(phoneNumberInput, "Invalid phone number");
+  } else {
+    clearErrorMessage(phoneNumberInput);
+    return true;
   }
 };
+// nameInput.addEventListener("input", isNameValidFn);
+// emailInput.addEventListener("input", isEmailValidFn);
+// passwordInput.addEventListener("input", isPasswordValidFn);
+// confirmPasswordInput.addEventListener("input", isConfirmPasswordValidFn);
+// phoneNumberInput.addEventListener("input", isPhoneNumberValidFn);
+// personalNumberInput.addEventListener("input", isPersonalNumberValidFn);
+// terms.addEventListener("change", isTermsValidFn);
